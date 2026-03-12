a2f500848b589d15a6075e2ed19935c872d55f87
arpit3134 <55105335+arpit3134@users.noreply.github.com>
2026-03-12 13:30:24 +0530

    Refactor Footer component into Card component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,7 +1,22 @@
-export default function Footer() {
+'use client';
+
+import React from 'react';
+import { classNames } from '@/lib/utils';
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+}
+
+export function Card({ className, children, ...props }: CardProps) {
   return (
-    <footer style={{marginTop:"40px", padding:"20px", borderTop:"1px solid #ddd"}}>
-      <p>© 2026 Openlov. All rights reserved.</p>
-    </footer>
+    <div
+      className={classNames(
+        'rounded-lg border border-gray-200 bg-white p-6 shadow-sm',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
   );
 }
