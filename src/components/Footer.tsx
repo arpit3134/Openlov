3bc0ed226cddabc2a23adabd7f6a83e1074257a0
arpit3134 <55105335+arpit3134@users.noreply.github.com>
2026-03-12 11:28:00 +0530

    Add Footer component with copyright notice

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{marginTop:"40px", padding:"20px", borderTop:"1px solid #ddd"}}>
+      <p>© 2026 Openlov. All rights reserved.</p>
+    </footer>
+  );
+}
